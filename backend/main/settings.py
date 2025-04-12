d85bf7ca148902404d3dc1ef2b4a36e8de66c28a
K Venkatesh <143533105+venkatesh21bit@users.noreply.github.com>
2025-04-12 23:01:11 +0530

    Update settings.py

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -83,9 +83,9 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'smartchain_db',
-        'USER': 'smartchain',
-        'PASSWORD': 'venkat*2005',
+        'NAME': 'your database name',
+        'USER': 'your username',
+        'PASSWORD': 'your password',
         'HOST': 'localhost',  # If running PostgreSQL locally
         'PORT': '5432',       # Default PostgreSQL port
     }
